236a1d326c9ac3aa0cabff0b0dcaf177da05557a
Git-Cat-21 <khushmish21@gmail.com>
2025-06-20 19:49:52 +0530

    redis and sentinels configs

---
+++ clustering/script.sh
@@ -0,0 +1,37 @@
+
+# Create Docker network (only once)
+#docker network create redis
+
+# redis-0
+docker run -d --name redis-0 \
+    --net redis \
+    -v ${PWD}/redis-0:/etc/redis/ \
+    redis:6.0-alpine redis-server /etc/redis/redis.conf
+
+# redis-1
+docker run -d --name redis-1 \
+    --net redis \
+    -v ${PWD}/redis-1:/etc/redis/ \
+    redis:6.0-alpine redis-server /etc/redis/redis.conf
+
+# redis-2
+docker run -d --name redis-2 \
+    --net redis \
+    -v ${PWD}/redis-2:/etc/redis/ \
+    redis:6.0-alpine redis-server /etc/redis/redis.conf
+
+
+docker run -d --name sentinel-0 --net redis \
+    -v ${PWD}/sentinel-0:/etc/redis/ \
+    redis:6.0-alpine \
+    redis-sentinel /etc/redis/sentinel.conf
+
+docker run -d --name sentinel-1 --net redis \
+    -v ${PWD}/sentinel-1:/etc/redis/ \
+    redis:6.0-alpine \
+    redis-sentinel /etc/redis/sentinel.conf
+
+docker run -d --name sentinel-2 --net redis \
+    -v ${PWD}/sentinel-2:/etc/redis/ \
+    redis:6.0-alpine \
+    redis-sentinel /etc/redis/sentinel.conf
